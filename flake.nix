69e1e6b30714df5d9d6939d01a4e3c76e80f0951
Martynas Mickevičius <self@2m.lt>
2024-12-09 22:24:12 +0200

    Nixify the thing

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.graphviz
+              pkgs.fish
+              pkgs.just
+            ];
+            buildInputs = with pkgs.python3Packages; [
+              networkx
+              pygraphviz
+              matplotlib
+            ];
+            shellHook = ''
+              fish
+            '';
+          };
+        }
+      );
+    };
+}
--- justfile
+++ justfile
@@ -1,14 +1,5 @@
-install-deps:
-    brew install graphviz
-    export GRAPHVIZ_PREFIX=$(brew --prefix graphviz)
-    export CFLAGS="-I$GRAPHVIZ_PREFIX/include"
-    export LDFLAGS="-L$GRAPHVIZ_PREFIX/lib"
-
-install:
-    pip install -r requirements.txt
-
-freeze:
-    pip freeze > requirements.txt
+dev:
+    nix develop
 
 run command:
     python sodai.py {{command}}
